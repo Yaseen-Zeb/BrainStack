e56b00941afe826da4c34f18a766fbc4fd1fa728
Yaseen-Zeb <25yaseen00@gmail.com>
2025-09-14 16:46:38 +0500

    add asynchronous file operations

--- nodeJs/practice/b   fs.js
+++ nodeJs/practice/b   fs.js
@@ -1,6 +1,6 @@
 // Import the built-in 'fs' (File System) module to work with files
 const fs = require("fs");
-
+//                                 ----------------Synchronous-----------------
 const txt = `Content written at ${new Date()}`;
 
 // Write the text into a file named 'file-a.txt' inside the 'files' folder
@@ -11,3 +11,23 @@ fs.writeFileSync("./files/file-a.txt", txt);
 const fileAContent = fs.readFileSync("./files/file-a.txt", "utf-8");
 
 console.log(fileAContent);
+
+// Note : In the above code, for error handling we can use (try,catch)
+
+//                                  ----------------Asynchronous-----------------
+
+fs.writeFile("./files/file-a.txt", txt, (writeErr) => {
+  if (writeErr) {
+    console.error("Error writing file:", writeErr);
+    return;
+  }
+  console.log("File written successfully.");
+
+  fs.readFile("./files/file-a.txt", "utf-8", (readErr, data) => {
+    if (readErr) {
+      console.error("Error reading file:", readErr);
+      return;
+    }
+    console.log("File content:", data);
+  });
+});
