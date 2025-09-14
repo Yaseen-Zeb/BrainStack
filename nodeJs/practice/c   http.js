eb89603db48a39c65476fbf5715d5bbbea5d56ea
Yaseen-Zeb <25yaseen00@gmail.com>
2025-09-14 20:29:30 +0500

    refactor: enhance HTTP server response handling and routing comments

--- nodeJs/practice/c   http.js
+++ nodeJs/practice/c   http.js
@@ -4,16 +4,28 @@ const http = require("http"); // retuern an object
 // Create an HTTP server instance
 const server = http.createServer((request, response) => {
   console.log("Request received");
-
+  //                          ---------------Routing-----------------
+  // You can also serve an HTML file instead of plain text.
+  // But if you reference external CSS or JS files using
+  // <link rel="stylesheet" href="..."> or <script src="..."></script>,
+  // you must also create separate routes or a static-file server to serve those files.
+  // Node's core 'http' module does NOT automatically handle static files.
   if (request.url === "/") {
-    response.end("index");
+    // Set HTTP response status and headers
+    // 200 → OK (successful request)
+    // 'Content-Type' → tells the browser the type of content being sent
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("index page");
+    return;
   }
 
   if (request.url === "/users") {
-    response.end("users");
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("users page");
+    return;
   }
-
-  response.end("General");
+  response.writeHead(404);
+  response.end("route not found");
 });
 
 server.listen(8000, "127.0.0.1", () => {
