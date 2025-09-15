94de663de16ea46536b18e715e807bc9372f9520
Yaseen-Zeb <25yaseen00@gmail.com>
2025-09-15 13:17:18 +0500

    add: custom module section

---
+++ nodeJs/practice/d  custom-module/app.js
@@ -0,0 +1,4 @@
+const math = require("./math"); // Import custom module
+
+console.log(math.add(2, 3));
+console.log(math.subtract(10, 4));
---
+++ nodeJs/practice/d  custom-module/math.js
@@ -0,0 +1,14 @@
+// math.js
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+// Export the functions so other files can use them
+module.exports = {
+  add,
+  subtract,
+};
